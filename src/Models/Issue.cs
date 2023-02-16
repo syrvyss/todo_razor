8de90430080a957c81f1f2e3f8efd48281c73a2f
Nikolaj Lübker <nikolaj.luebker@lego.com>
2023-02-16 20:20:23 +0100

    Added fields to Issue

--- src/Models/Issue.cs
+++ src/Models/Issue.cs
@@ -3,8 +3,26 @@ namespace WebApp.Models;
 public class Issue : IGuid {
     public Guid Id { get; } = Guid.NewGuid();
     public DateTime Created { get; } = DateTime.Now;
-    public Status Status { get; set; }
-    public Priority Priority { get; set; }
+    public required Status Status { get; set; }
+    public required Priority Priority { get; set; }
+
+    public string Name {
+        get { return this.Name; }
+        set {
+            if (value.Length > 20) {
+                Name = value;
+            }
+        }
+    }
+
+    public string Description {
+        get { return this.Description; }
+        set {
+            if (value.Length > 100) {
+                Description = value;
+            }
+        }
+    }
 }
 
 public enum Priority {

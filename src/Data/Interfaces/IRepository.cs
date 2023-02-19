4968a477b0bec106d69f0fa3a5c8eae072600db3
syrvyss <lybkern@gmail.com>
2023-02-19 17:05:29 +0100

    string -> Guid

--- src/Data/Interfaces/IRepository.cs
+++ src/Data/Interfaces/IRepository.cs
@@ -2,8 +2,8 @@ namespace WebApp.Data.Interfaces;
 
 public interface IRepository<T> {
     public void Create(T item);
-    public void Delete(string id);
-    public T? Read(string id);
+    public void Delete(Guid id);
+    public T? Read(Guid id);
     public List<T> Get();
-    public void Update(string id, T thing);
+    public void Update(Guid id, T thing);
 }

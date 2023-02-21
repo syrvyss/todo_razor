d9993f07ee1fb627f47ed579f8ce7664ee79ab24
Nikolaj Lübker <nikolaj.luebker@lego.com>
2023-02-21 20:07:21 +0100

    Moved Backlog to its own page, replaced index with basic h1 and p

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -5,16 +5,11 @@ namespace WebApp.Pages;
 
 public class IndexModel : PageModel {
     private readonly ILogger<IndexModel> _logger;
-    private readonly IIssueRepository _issueRepository;
 
-    public List<Issue> Issues { get; set; } = new();
-
-    public IndexModel(ILogger<IndexModel> logger, IIssueRepository issueRepository) {
+    public IndexModel(ILogger<IndexModel> logger) {
         _logger = logger;
-        _issueRepository = issueRepository;
     }
 
     public void OnGet() {
-        Issues = _issueRepository.Get();
     }
 }

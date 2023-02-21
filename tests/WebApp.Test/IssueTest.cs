85dae61203540851428aba5313e8ca415df582e9
Nikolaj Lübker <nikolaj.luebker@lego.com>
2023-02-21 11:06:55 +0100

    Added xUnit tests

---
+++ tests/WebApp.Test/IssueTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Microsoft.AspNetCore.Http.HttpResults;
+using WebApp.Data.Interfaces;
+using WebApp.Models;
+
+using Xunit;
+
+namespace WebApp.Test {
+    public class IssueTest {
+        [Fact]
+        public void IsCorrectDate() {
+            // Arrange
+            Issue issue = new() {
+                Status = Status.Todo,
+                Priority = Priority.Low
+            };
+
+            // Act
+            int dayOfYear = issue.Created.Value.DayOfYear;
+
+            // Assert
+            Assert.Equal(dayOfYear, issue.Created.Value.DayOfYear);
+        }
+
+        [Fact]
+        public void IsCorrectId() {
+            // Arrange
+            List<Issue> issues = new();
+
+            // Act
+            for (int i = 0; i < 255; i++) {
+                Issue temp = new() {
+                    Status = Status.Doing,
+                    Priority = Priority.Normal
+                };
+                
+                issues.Add(temp);
+            }
+
+            // Assert
+            Assert.True(!issues.Distinct().Any());
+        }
+    }
+}

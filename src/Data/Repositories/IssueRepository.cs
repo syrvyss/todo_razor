24139c822f3252f425324aa837ebf6921acd8846
Nikolaj Lübker <nikolaj.luebker@lego.com>
2023-03-09 12:13:57 +0100

    Override repository methods to communicate with db

--- src/Data/Repositories/IssueRepository.cs
+++ src/Data/Repositories/IssueRepository.cs
@@ -1,7 +1,68 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
 using WebApp.Data.Interfaces;
-using WebApp.Models;
 
 namespace WebApp.Data.Repositories;
 
 public class IssueRepository : BaseRepository<Issue>, IIssueRepository {
+    public void Delete(Guid id) {
+        var collection = GetValues();
+
+        var filter = Builders<BsonDocument>.Filter
+            .Eq(x => "_id", id.ToString());
+
+        collection?.DeleteOne(filter);
+    }
+
+    public List<Issue> Get() {
+        var collection = GetValues();
+
+        return collection
+            .AsQueryable()
+            .ToList()
+            .ConvertAll(x => BsonSerializer.Deserialize<Issue>(x.ToBsonDocument()));
+    }
+
+    public void Update(Issue issue) {
+        var collection = GetValues();
+
+        var filter = Builders<BsonDocument>.Filter
+            .Eq(x => "_id", issue.Id?.ToString());
+
+        var update = Builders<BsonDocument>.Update
+            .Set(x => x, issue.ToBsonDocument());
+
+        collection?.UpdateOne(filter, update);
+    }
+
+    public Issue? Read(Guid id) {
+        var collection = GetValues();
+
+        var obj = collection.Find(x => x["_id"] == id);
+        return BsonSerializer.Deserialize<Issue>(obj.ToBsonDocument());
+    }
+
+    public void Create(Issue issue) {
+        var collection = GetValues();
+        var document = new BsonDocument(issue.ToBsonDocument());
+
+        collection?.InsertOne(document);
+        Console.WriteLine(collection);
+    }
+
+    private IMongoCollection<BsonDocument>? GetValues() {
+        var connectionString = Environment.GetEnvironmentVariable("MONGODB_URI");
+
+        if (connectionString == null) {
+            Console.WriteLine(
+                "You must set your 'MONGODB_URI' environmental variable. See\n\t https://www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable");
+            Environment.Exit(0);
+        }
+
+        var client = new MongoClient(connectionString);
+        var collection = client.GetDatabase("todo_razor").GetCollection<BsonDocument>("issue");
+
+        return collection;
+    }
 }

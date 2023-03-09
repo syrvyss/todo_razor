26253e42fa1695b644e28f27dc4ff292c227f8be
Nikolaj Lübker <nikolaj.luebker@lego.com>
2023-03-09 12:14:06 +0100

    User class for future reference

---
+++ src/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApp.Models;
+
+public class User : IGuid {
+    public User() {
+        Id ??= Guid.NewGuid();
+    }
+
+    public string Username { get; set; }
+    public string Password { get; set; }
+
+    public Guid? Id { get; init; }
+}
